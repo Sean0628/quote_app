ca67fe229b2daff14d1b6a7ef359584f629aa3f6
Sho Ito <i.sho.0628@gmail.com>
2019-02-03 19:10:00 +0900

    mod status bar

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,7 +5,7 @@
       <div class="progress">
         <div class='progress'>
           <div class="progress-bar" role='progressbar' aria-valuenow='60' aria-valuemin='0' aria-valuemax='100'
-               :style="{width: (quoteCount / maxCounts) * 100 + '%'}">
+               :style="{width: (quoteCount / maxQuotes) * 100 + '%'}">
             {{ quoteCount }} / {{ maxQuotes }}
           </div>
         </div>
